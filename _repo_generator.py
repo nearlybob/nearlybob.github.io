c83130e3b4dd810faac7e401205754f1bf6f464d
nearlybob <justforkodimedia@gmail.com>
2026-09-07 15:56:45 +0100

    update repo_generator.py

--- _repo_generator.py
+++ _repo_generator.py
@@ -13,7 +13,7 @@ import zipfile
 from xml.etree import ElementTree
 
 SCRIPT_VERSION = 5
-KODI_VERSIONS = ["krypton", "leia", "matrix", "nexus", "repo"]
+KODI_VERSIONS = ["repo", "piers"]
 IGNORE = [
     ".git",
     ".github",
